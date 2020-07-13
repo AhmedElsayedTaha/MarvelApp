bcb682a4db995f41d7384e9fd51bf311fb80690e
Ahmed <ahmedelsayedtahaa@gmail.com>
2020-07-13 11:42:43 +0200

    new branch test

--- app/src/main/java/com/marvelapp/AvengersDetails.java
+++ app/src/main/java/com/marvelapp/AvengersDetails.java
@@ -14,6 +14,7 @@ import butterknife.ButterKnife;
 
 public class AvengersDetails extends AppCompatActivity {
 
+    //Hello new branch
     @BindView(R.id.img)
     ImageView img;
     @BindView(R.id.name)
